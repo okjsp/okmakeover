ce33ed6e8febbe0eea713f60fcdb8fb476f69f85
jth8172 <jth8172@nate.com>
2014-01-04 14:45:46 +0900

    vo 생성

---
+++ src/main/java/net/okjsp/commboard/model/BoardattachVO.java
@@ -0,0 +1,35 @@
+package net.okjsp.commboard.model;
+
+public class BoardattachVO {
+
+	// 첨부ID
+	private String attchId;
+	
+	// 첨부순서
+	private String attchOrder;
+	
+	// 파일명
+	private String fileName;
+	
+	// 저장파일명
+	private String saveFilename;
+	
+	// 파일타입
+	private String fileType;
+	
+	// 파일용량
+	private String fileSize;
+	
+	// 그림높이
+	private String imgHeight;
+	
+	// 그림넓이
+	private String imgWidth;
+	
+	// 등록일시
+	private String writeDate;
+	
+	// 변경일시
+	private String updateDate;
+	
+}
