e69bbbe863274e9674636848bdc9c6948deb0abe
jth8172 <jth8172@nate.com>
2014-01-06 23:09:44 +0900

    model commit

---
+++ src/main/java/net/okjsp/commboard/model/Comment.java
@@ -0,0 +1,63 @@
+package net.okjsp.commboard.model;
+
+public class CommentVO {
+	//게시판 번호
+	private int board_no;
+	//글 번호
+	private int write_no;
+	//코멘트 번호
+	private int comment_id;
+	//닉네임
+	private String nick_name;
+	//본문
+	private String content;
+	//올린 시간
+	private String write_date;
+	//수정 시간
+	private String update_date;
+	
+	public int getBoard_no() {
+		return board_no;
+	}
+	public void setBoard_no(int board_no) {
+		this.board_no = board_no;
+	}
+	public int getWrite_no() {
+		return write_no;
+	}
+	public void setWrite_no(int write_no) {
+		this.write_no = write_no;
+	}
+	public int getComment_id() {
+		return comment_id;
+	}
+	public void setComment_id(int comment_id) {
+		this.comment_id = comment_id;
+	}
+	public String getNick_name() {
+		return nick_name;
+	}
+	public void setNick_name(String nick_name) {
+		this.nick_name = nick_name;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getWrite_date() {
+		return write_date;
+	}
+	public void setWrite_date(String write_date) {
+		this.write_date = write_date;
+	}
+	public String getUpdate_date() {
+		return update_date;
+	}
+	public void setUpdate_date(String update_date) {
+		this.update_date = update_date;
+	}
+	
+	
+}
