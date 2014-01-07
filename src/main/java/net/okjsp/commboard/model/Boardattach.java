916a9e726543fd7b78c7ebf1beeae0335aba0c95
Roy Jung <jwjung@okjsp.net>
2014-01-07 21:00:03 +0900

    A조 업데이

---
+++ src/main/java/net/okjsp/commboard/model/Boardattach.java
@@ -0,0 +1,117 @@
+package net.okjsp.commboard.model;
+
+public class Boardattach {
+
+	// 첨부ID
+	private String attchId;
+	
+	// 첨부순서
+	private String attchOrder;
+	
+	// 파일명
+	private String fileName;
+	
+	// 저장파일명
+	private String saveFilename;
+	
+	// 파일타입
+	private String fileType;
+	
+	// 파일용량
+	private String fileSize;
+	
+	// 그림높이
+	private String imgHeight;
+	
+	// 그림넓이
+	private String imgWidth;
+	
+	// 등록일시
+	private String writeDate;
+	
+	// 변경일시
+	private String updateDate;
+
+	public String getAttchId() {
+		return attchId;
+	}
+
+	public void setAttchId(String attchId) {
+		this.attchId = attchId;
+	}
+
+	public String getAttchOrder() {
+		return attchOrder;
+	}
+
+	public void setAttchOrder(String attchOrder) {
+		this.attchOrder = attchOrder;
+	}
+
+	public String getFileName() {
+		return fileName;
+	}
+
+	public void setFileName(String fileName) {
+		this.fileName = fileName;
+	}
+
+	public String getSaveFilename() {
+		return saveFilename;
+	}
+
+	public void setSaveFilename(String saveFilename) {
+		this.saveFilename = saveFilename;
+	}
+
+	public String getFileType() {
+		return fileType;
+	}
+
+	public void setFileType(String fileType) {
+		this.fileType = fileType;
+	}
+
+	public String getFileSize() {
+		return fileSize;
+	}
+
+	public void setFileSize(String fileSize) {
+		this.fileSize = fileSize;
+	}
+
+	public String getImgHeight() {
+		return imgHeight;
+	}
+
+	public void setImgHeight(String imgHeight) {
+		this.imgHeight = imgHeight;
+	}
+
+	public String getImgWidth() {
+		return imgWidth;
+	}
+
+	public void setImgWidth(String imgWidth) {
+		this.imgWidth = imgWidth;
+	}
+
+	public String getWriteDate() {
+		return writeDate;
+	}
+
+	public void setWriteDate(String writeDate) {
+		this.writeDate = writeDate;
+	}
+
+	public String getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(String updateDate) {
+		this.updateDate = updateDate;
+	}
+	
+	
+	
+}
---
+++ src/main/java/net/okjsp/commboard/model/Recommendation.java
@@ -0,0 +1,63 @@
+package net.okjsp.commboard.model;
+
+public class Recommendation {
+	//게시판 번호
+	private int borad_id;
+	//글 번호
+	private int write_no;
+	//덧글 번호
+	private int comment_id;
+	//구분 아이디???
+	private String guboon_id;
+	//뭐지...
+	private String intpropose_id;
+	//추천 점수
+	private String score;
+	//추천 날짜
+	private String recommendation_date;
+	public int getBorad_id() {
+		return borad_id;
+	}
+	public void setBorad_id(int borad_id) {
+		this.borad_id = borad_id;
+	}
+	public int getWrite_no() {
+		return write_no;
+	}
+	public void setWrite_no(int write_no) {
+		this.write_no = write_no;
+	}
+	public int getComment_id() {
+		return comment_id;
+	}
+	public void setComment_id(int comment_id) {
+		this.comment_id = comment_id;
+	}
+	public String getGuboon_id() {
+		return guboon_id;
+	}
+	public void setGuboon_id(String guboon_id) {
+		this.guboon_id = guboon_id;
+	}
+	public String getIntpropose_id() {
+		return intpropose_id;
+	}
+	public void setIntpropose_id(String intpropose_id) {
+		this.intpropose_id = intpropose_id;
+	}
+	public String getScore() {
+		return score;
+	}
+	public void setScore(String score) {
+		this.score = score;
+	}
+	public String getRecommendation_date() {
+		return recommendation_date;
+	}
+	public void setRecommendation_date(String recommendation_date) {
+		this.recommendation_date = recommendation_date;
+	}
+	
+	
+
+}
