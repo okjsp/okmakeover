03a6ba4f7773477598dd2835d33d464253d4f666
jth8172 <jth8172@nate.com>
2014-01-13 23:19:14 +0900

    VO 수정 및 댓글 서버사이드

--- src/main/java/net/okjsp/commboard/model/Attachmapping.java
+++ src/main/java/net/okjsp/commboard/model/Attachmapping.java
@@ -2,45 +2,47 @@ package net.okjsp.commboard.model;
 
 public class Attachmapping {
 	//게시판 번호
-	private int board_id;
+	private int boardId;
 	//글 번호
-	private int write_no;
+	private int writeNo;
 	//코멘트 번호
-	private int comment_id;
+	private int commentId;
 	//첨부파일 아이디
-	private int attach_id;
+	private int attachId;
 	//첨부파일 순서
-	private int attach_order;
-	public int getBoard_id() {
-		return board_id;
-	}
-	public void setBoard_id(int board_id) {
-		this.board_id = board_id;
-	}
-	public int getWrite_no() {
-		return write_no;
-	}
-	public void setWrite_no(int write_no) {
-		this.write_no = write_no;
-	}
-	public int getComment_id() {
-		return comment_id;
-	}
-	public void setComment_id(int comment_id) {
-		this.comment_id = comment_id;
-	}
-	public int getAttach_id() {
-		return attach_id;
-	}
-	public void setAttach_id(int attach_id) {
-		this.attach_id = attach_id;
-	}
-	public int getAttach_order() {
-		return attach_order;
-	}
-	public void setAttach_order(int attach_order) {
-		this.attach_order = attach_order;
-	}
+	private int attachOrder;
 	
+	
+	public int getBoardId() {
+		return boardId;
+	}
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
+	public int getWriteNo() {
+		return writeNo;
+	}
+	public void setWriteNo(int writeNo) {
+		this.writeNo = writeNo;
+	}
+	public int getCommentId() {
+		return commentId;
+	}
+	public void setCommentId(int commentId) {
+		this.commentId = commentId;
+	}
+	public int getAttachId() {
+		return attachId;
+	}
+	public void setAttachId(int attachId) {
+		this.attachId = attachId;
+	}
+	public int getAttachOrder() {
+		return attachOrder;
+	}
+	public void setAttachOrder(int attachOrder) {
+		this.attachOrder = attachOrder;
+	}
+
 	
 }
